d10c0af82437a11a4e4913ce6d945af8c2269efd
Samuel Pacheco Pereira <mucapacheco@hotmail.com>
2020-02-26 11:41:16 -0300

    Publicando para o git

--- src/composite/compite.js
+++ src/composite/compite.js
@@ -1,4 +1,5 @@
 import React  from 'react';
+import ReactTooltip from 'react-tooltip' ;
 
 function Composite(props){
 
@@ -6,11 +7,12 @@ function Composite(props){
         return '';
     }
 
-    return <div className={"main "+(props.selecionados && props.selecionados.includes(props.data.name)?'ativo':'')}>
-        <div>{props.data.name}</div>
-        <div className={"children "+props.data.type}>
+    return <div  className={"main "+(props.selecionados && props.selecionados.includes(props.data.name)?'ativo':'')}>
+        <div data-tip={props.logs[props.data.name] && props.logs[props.data.name].join("<br/>") }>{props.data.name}</div>
+        <div className={"children "+props.data.type}  >
             {props.data.children && props.data.children.map((item,key) => <Composite logs={props.logs} selecionados={props.selecionados} key={key} data={item} />)}
         </div>
+        <ReactTooltip />
     </div>
 
 }
